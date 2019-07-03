2007e3ce006efb8dd1b28096b0b9a1d33f222609
LuisLiraC <luis_ssbm@hotmail.com>
2019-07-02 22:21:06 -0500

    DB Connect

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient
 const { DB_HOST, DB_PORT, DB_NAME } = require('./config')
-const connString = `mongodb://${DB_HOST}:${DB_PORT}`
+const connString = process.env.MONGODB_URI || `mongodb://${DB_HOST}:${DB_PORT}`
 
 let instance = null
 let isDisconnecting = false
