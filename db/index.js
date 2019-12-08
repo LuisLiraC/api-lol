2d2636fcee873dec4e3087ce10b5a9f9d91ed384
LuisLiraC <luis_ssbm@hotmail.com>
2019-12-07 20:20:15 -0600

    Http methods: put, post and deleted added

--- db/index.js
+++ db/index.js
@@ -10,7 +10,37 @@ module.exports = {
   },
   getChampionByName: (name) => {
     const db = mongo.instance().db(DB_NAME)
-    const data = db.collection('champions').find({ name: name }).toArray()
+    const data = db.collection('champions').findOne({ name: name })
     return data
+  },
+  createChampion: () => {
+    const createdChampion = new Promise((res, rej) => {
+      setTimeout(() => {
+        res({ 
+          result: "champion created"
+        })
+      }, 2000);
+    })
+    return createdChampion
+  },
+  deleteChampion: () => {
+    const deletedChampion = new Promise((res, rej) => {
+      setTimeout(() => {
+        res({
+          result: "champion deleted"
+        })
+      }, 2000);
+    })
+    return deletedChampion
+  },
+  updateChampion: () => {
+    const updatedChampion = new Promise((res, rej) => {
+      setTimeout(() => {
+        res({
+          result: "champion updated"
+        })
+      }, 2000);
+    })
+    return updatedChampion
   }
 }
--- routes/api/champions.js
+++ routes/api/champions.js
@@ -1,5 +1,5 @@
 const API_PATH = '/api'
-const { getChampions, getChampionByName } = require('../../db')
+const { getChampions, getChampionByName, createChampion, deleteChampion, updateChampion } = require('../../db')
 
 module.exports = (app) => {
   app.get(`${API_PATH}`, (req, res) => {
@@ -7,25 +7,51 @@ module.exports = (app) => {
   })
 
   app.get(`${API_PATH}/champions`, async (req, res, next) => {
-
     try {
       const champions = await getChampions()
       res.json(champions)
     } catch (error) {
       next(error)
     }
-
   })
 
   app.get(`${API_PATH}/champions/:name`, async (req, res, next) => {
     const name = req.params.name.toLowerCase()
-
     try {
       const champion = await getChampionByName(name)
-      res.send(champion[0])
+      res.send(champion)
     } catch (error) {
       next(error)
     }
+  })
 
+  app.post(`${API_PATH}/champions`, async (req, res, next) => {
+    try {   
+      const createdChampion = await createChampion()
+      res.statusCode = 201
+      res.send(createdChampion)
+    } catch(err) {
+      next(err)
+    }
+  })
+
+  app.delete(`${API_PATH}/champions/:name`, async (req, res, next) => {
+    try {
+      const deletedChampion = await deleteChampion()
+      res.statusCode = 200
+      res.send(deletedChampion)
+    } catch(err) {
+      next(err)
+    }
+  })
+
+  app.put(`${API_PATH}/champions/:name`, async (req, res, next) => {
+    try {
+      const updatedChampion = await updateChampion()
+      res.statusCode = 201
+      res.send(updatedChampion)
+    } catch(err) {
+      next(err)
+    }
   })
 }
